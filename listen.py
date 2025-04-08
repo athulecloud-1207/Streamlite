e19cf83f024aa1c50455964c5617dc2b44bf490a
athulecloud-1207 <asunil@ecloudcontrol.com>
2025-04-08 16:13:11 +0530

    Update listen.py

--- listen.py
+++ listen.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import random
 
-st.set_page_config(page_title="Random Quote Generator", page_icon="📝")
+st.set_page_config(page_title="Random Quote Generator", page_icon="📝", layout="centered")
 
+# Title with emoji and spacing
 st.title("📝 Random Quote Generator")
+st.markdown("### Get your daily dose of inspiration!")
 
+# Spacer
+st.markdown("---")
+
+# List of quotes
 quotes = [
     "Believe in yourself.",
     "Stay hungry, stay foolish.",
@@ -14,7 +20,16 @@ quotes = [
     "Whatever you are, be a good one.",
 ]
 
-if st.button("✨ Show me a quote"):
-    st.success(random.choice(quotes))
-else:
-    st.write("Click the button to get inspired!")
+# Layout using columns to center the button
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    if st.button("✨ Show me a quote"):
+        quote = random.choice(quotes)
+        st.markdown("#### 💬 Here's your quote:")
+        st.success(f"_{quote}_")
+    else:
+        st.info("Click the button to get inspired!")
+
+# Footer spacer
+st.markdown("---")
+st.caption("Powered by Streamlit")
